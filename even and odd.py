996778fbb90f952a52f28941b4dd7152607f6b11
Naresh <banothnaresh004@gmail.com>
2021-04-06 13:16:54 +0530

    Add all my files

--- even and odd.py
+++ even and odd.py
@@ -1,6 +1,6 @@
 def fibbonaci():
-    a=int(input())
-    b=int(input())
+    a=int(input("Enter the first number"))
+    b=int(input("Enter the second number"))
     for i in range(10):
         c=a+b
         a=b
